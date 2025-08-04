f6960e989e36b16404a2d050bbbacae2ae1e0eb2
yuichiyasui <yassan.b.a.w@gmail.com>
2025-08-05 00:30:06 +0900

    stylesディレクトリを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "D3 Practice",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>D3 Practice</h1>
+      <h1 className="text-lg font-bold">D3 Practice</h1>
     </main>
   );
 }
